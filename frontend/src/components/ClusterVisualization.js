c40e3ab93c5e0721970f8ae8d2dc4d3b7f56f31c
SOHAMPAL23 <palsoham074@gmail.com>
2026-01-06 20:29:58 +0530

    cluster

--- frontend/src/components/ClusterVisualization.js
+++ frontend/src/components/ClusterVisualization.js
@@ -1,20 +1,37 @@
 import React from 'react';
+import './ClusterVisualization.css';
 
-const ClusterVisualization = ({ clusters }) => {
+const ClusterVisualization = ({ clusters, onClusterSelect, selectedCluster }) => {
+  const handleClusterClick = (cluster) => {
+    onClusterSelect(cluster);
+  };
+
   return (
     <div className="cluster-visualization">
       <h3>Movie Clusters</h3>
       {clusters.length > 0 ? (
-        <div className="clusters-container">
-          {clusters.map((cluster) => (
-            <div key={cluster.id} className="cluster-item">
-              <span className="cluster-name">{cluster.name}</span>
-              <span className="cluster-count">{cluster.count} movies</span>
+        clusters.map((cluster) => (
+          <div
+            key={cluster.id}
+            className={`cluster-item ${selectedCluster && selectedCluster.id === cluster.id ? 'selected' : ''}`}
+            onClick={() => handleClusterClick(cluster)}
+            title={`${cluster.name} - ${cluster.count} movies`}
+          >
+            <h4>{cluster.name}</h4>
+            <p>{cluster.count} movies</p>
+            <div className="cluster-movies-preview">
+              {cluster.movies.slice(0, 3).map((movie) => (
+                <span key={movie.id} className="movie-preview">
+                  {movie.title.split(' ')[0]}
+                </span>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        ))
       ) : (
-        <div className="no-clusters">Loading clusters...</div>
+        <div className="no-clusters">
+          <p>Loading clusters...</p>
+        </div>
       )}
     </div>
   );
